dc69960f568d432e2451b91b42b3a7ad71a2b207
emacca <emacca@me.com>
2014-02-03 15:25:45 +1100

    added column to exercises category an updated databse migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140203034313) do
+ActiveRecord::Schema.define(version: 20140203042316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140203034313) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
   create_table "exercises_workouts", id: false, force: true do |t|
