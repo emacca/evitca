4f26c3e74d8bcf843a527ad7ab3ee3f18045e93a
emacca <emacca@me.com>
2014-02-04 15:53:42 +1100

    Added a index page without a sign up from on the front, chnaged the seed, removed new exercise button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, :except => [:index]
   def home    
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,50 +11,50 @@ user.save
 Workout.create(
   :day => 'Monday',
   :name => 'Cardio',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Workout.create(
   :day => 'Tuesday',
   :name => 'Legs Day',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Workout.create(
   :day => 'Wednesday',
   :name => 'Arms Day',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Workout.create(
   :day => 'Thursday',
   :name => 'Squats',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Workout.create(
   :day => 'Friday',
   :name => 'Cardio',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Workout.create(
   :day => 'Saturday',
   :name => 'HIIT',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Workout.create(
   :day => 'Sunday',
   :name => 'LISS',
-  :user_id => User.first
+  :user_id => User.first.id
 )
 
 Exercise.create(
   :name => 'Push Ups',
   :image => 'http://www.weightloss-tips4u.com/wp-content/uploads/2012/07/classic-push-up_push-up-variations.jpg',
   :description => 'The push up may just be the perfect total body exercise that builds both upper body and core strength. Done properly, it is a compound exercise that uses muscles in the chest, shoulders, triceps, back, abs and even the legs.',
-  :category => 'Tricep'
+  :category => 'Upper Body'
 )
 
 
@@ -62,14 +62,14 @@ Exercise.create(
   :name => 'Bicep Curl',
   :image => 'http://www.menshealth.co.uk/cm/menshealthuk/images/pV/DB-bicep-curl-plan-b-13062012-de.jpg',
   :description => 'The push up may just be the perfect total body exercise that builds both upper body and core strength. Done properly, it is a compound exercise that uses muscles in the chest, shoulders, triceps, back, abs and even the legs.',
-  :category => 'Bicep'
+  :category => 'Upper Body'
 )
 
 Exercise.create(
   :name => 'Push Press',
   :image => 'http://www.womenshealthmag.com/files/images/0609_push_press.jpg',
   :description => 'The push up may just be the perfect total body exercise that builds both upper body and core strength. Done properly, it is a compound exercise that uses muscles in the chest, shoulders, triceps, back, abs and even the legs.',
-  :category => 'shoulders'
+  :category => 'Upper Body'
 )
 
 
@@ -77,5 +77,12 @@ Exercise.create(
   :name => 'Chest Press',
   :image => 'http://www.womenshealthmag.com/files/images/0906-dbell-chest-press.jpg',
   :description => 'The push up may just be the perfect total body exercise that builds both upper body and core strength. Done properly, it is a compound exercise that uses muscles in the chest, shoulders, triceps, back, abs and even the legs.',
-  :category => 'Chest'
+  :category => 'Upper Body'
+)
+
+Exercise.create(
+  :name => 'Squats',
+  :image => 'http://www.womenshealthmag.com/files/images/0906-dbell-chest-press.jpg',
+  :description => 'The push up may just be the perfect total body exercise that builds both upper body and core strength. Done properly, it is a compound exercise that uses muscles in the chest, shoulders, triceps, back, abs and even the legs.',
+  :category => 'Lower Body'
 )
