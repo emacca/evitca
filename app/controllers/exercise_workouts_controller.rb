c235cf078d772e0757a5b6050003ddae25cf0205
emacca <emacca@me.com>
2014-02-06 11:54:15 +1100

    added styles for exercises, workouts and forms, buttons

--- app/controllers/exercise_workouts_controller.rb
+++ app/controllers/exercise_workouts_controller.rb
@@ -1,26 +1,26 @@
 class ExerciseWorkoutsController < ApplicationController
   before_action :set_exercise_workouts, only: [:edit, :update]
 
-def edit
-end
-
-def update 
-
-  if @exercise_workouts.update(exercise_workouts_params)
-  redirect_to @exercise_workouts.workout, notice: 'Exercise was successfully updated.' 
-  else
-  render action: 'edit' 
+  def edit
   end
 
-end
+  def update 
 
-private 
-
-def set_exercise_workouts
-  @exercise_workouts = ExerciseWorkout.find(params[:id]) 
-end
-
-def exercise_workouts_params
-      params.require(:exercise_workout).permit(:reps, :times, :exercise_id, :workout_id)
+    if @exercise_workouts.update(exercise_workouts_params)
+    redirect_to @exercise_workouts.workout, notice: 'Exercise was successfully updated.' 
+    else
+    render action: 'edit' 
     end
+
+  end
+
+  private 
+
+  def set_exercise_workouts
+    @exercise_workouts = ExerciseWorkout.find(params[:id]) 
+  end
+
+  def exercise_workouts_params
+    params.require(:exercise_workout).permit(:reps, :times, :exercise_id, :workout_id)
+  end
 end
