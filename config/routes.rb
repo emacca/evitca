2f14c2f528e553ab6ec0c3b0a3972550872ee3f6
emacca <emacca@me.com>
2014-02-26 01:44:56 +1100

    added / to a link

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Evitca::Application.routes.draw do
   resources :workouts
   resources :exercise_workouts
 
+  
   root :to => 'pages#home'
 
-  get ':action' => "pages#:action"
+  get '/:action' => "pages#:action"
 
 end
